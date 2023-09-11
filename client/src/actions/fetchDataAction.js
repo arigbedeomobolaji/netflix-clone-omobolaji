09d87eb753d3060eba148f1be825446ec045ca24
unknown <arigbedeomobolaji@gmail.com>
2023-09-11 13:48:51 +0100

    Add api remote route

--- client/src/actions/fetchDataAction.js
+++ client/src/actions/fetchDataAction.js
@@ -1,14 +1,19 @@
-import axios from "axios"
+import axios from "axios";
 
 export const fetchDataAction = () => {
-  return async (dispatch) => {
-    dispatch({type: "FETCH_DATA_REQUEST"})
-    try{
-      const {data} = await axios.get("/seed")
-      dispatch({type: "FETCH_DATA_SUCCESS", payload: data})
-    }catch(err){
-      const message = err.response && err.response.data.err ? err.response.data.err : err.message
-      dispatch({type: "FETCH_DATA_ERROR", payload: message}) 
-    }
-  }
-}
+	return async (dispatch) => {
+		dispatch({ type: "FETCH_DATA_REQUEST" });
+		try {
+			const { data } = await axios.get(
+				"https://netflix-clone-omobolaji-api.vercel.app/seed"
+			);
+			dispatch({ type: "FETCH_DATA_SUCCESS", payload: data });
+		} catch (err) {
+			const message =
+				err.response && err.response.data.err
+					? err.response.data.err
+					: err.message;
+			dispatch({ type: "FETCH_DATA_ERROR", payload: message });
+		}
+	};
+};
